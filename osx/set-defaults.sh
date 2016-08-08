d2c21a99920dd02b7bc00930d08d9d894116efe1
Jonathan Stacks <jonstacks13@gmail.com>
2016-08-08 13:37:58 -0500

    Add defaults for screensaver and battery
    Cause ya know...security

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -30,3 +30,11 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Show remaining battery time; show percentage
+defaults write com.apple.menuextra.battery ShowPercent YES
+defaults write com.apple.menuextra.battery ShowTime YES
+
+# Screensaver requires password; no delay before asking
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
