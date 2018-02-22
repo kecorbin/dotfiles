0b31de9d23f430fd09e445483e48122241a5c0ab
Kevin Corbin <kecorbin@cisco.com>
2018-02-22 08:28:46 -0600

    add python path for venvwrapper

--- python/virtualenvwrapper.zsh
+++ python/virtualenvwrapper.zsh
@@ -1,4 +1,6 @@
 export WORKON_HOME=~/VirtualEnvs
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+
 mkdir -p $WORKON_HOME
 cp ~/.dotfiles/python/hooks/* $WORKON_HOME
 source /usr/local/bin/virtualenvwrapper.sh
